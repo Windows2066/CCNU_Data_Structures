d0e563a686972914ca1c161b2a6135c31fd283ad
Windows2066 <windows2066@gmail.com>
2024-10-21 16:34:26 +0800

    fix bug

--- unit-4/esp7/QueueApplication.cpp
+++ unit-4/esp7/QueueApplication.cpp
@@ -8,19 +8,36 @@ int main()
     vector<int> a(n);
     for (auto &i : a)
         cin >> i;
-    unordered_map<int, queue<int>> mp;
+    unordered_map<int, deque<int>> mp;
     for (int i = 0; i < n; i++)
-        mp[a[i]].push(i);
+        mp[a[i]].push_back(i);
     int ans = 0;
-    for (auto [x, q] : mp)
+    for (auto &[x, pos] : mp)
     {
-        if (q.size() < 2)
+        if (pos.size() < 2)
             continue;
-        int prev = q.front();
-        while (q.size() > 1)
-            q.pop();
-        int curr = q.front();
-        ans = max(ans, curr - prev);
+        while (pos.size() > 1)
+        {
+            int first = pos.front();
+            pos.pop_front();
+            for (auto second : pos)
+            {
+                bool judge = true;
+                for (int k = first + 1; k < second; k++)
+                {
+                    if (a[k] < x)
+                    {
+                        judge = false;
+                        break;
+                    }
+                }
+                if (judge)
+                {
+                    ans = max(ans, second - first);
+                }
+            }
+        }
     }
     cout << ans << endl;
+    return 0;
 }
